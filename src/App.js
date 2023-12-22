65d3f02ea22fb0eda766c2aa0062cfcf8278f08b
deadshot002 <95534508+deadshot002@users.noreply.github.com>
2023-12-22 11:14:37 +0530

    commit5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Nav from './componennts/Nav';
 import SectionA from './componennts/SectionA';
+import SectionB from './componennts/SectionB';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Nav/>
     <SectionA/>
+    <SectionB/>
     </>
   );
 }
